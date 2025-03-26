c64fb168295bb2b15a3c20f1e52cf88043cf5dea
MiksMiks2005 <Miks.Svaheimers@edu.tukums.lv>
2025-03-26 15:12:18 +0200

    sss

--- Flaskus NIBUS/KAPECFFFFLASK.py
+++ Flaskus NIBUS/KAPECFFFFLASK.py
@@ -1,13 +1,26 @@
-from flask import Flask , render_template
+# from flask import Flask , render_template
+# app = Flask(__name__)
+# @app.route('/')
+# def home():
+#     return "<html><body><h1>Hello World</h1></body></html>"
+# @app.route('/hello/')
+# def hello_name(name):
+#    return 'Hello %s!' % name
+# @app.route('/pirmais')
+# def index():
+#    return render_template('pirmais.html')
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+
+
+from flask import Flask, render_template
 app = Flask(__name__)
 @app.route('/')
 def home():
-    return "<html><body><h1>Hello World</h1></body></html>"
-@app.route('/hello/')
-def hello_name(name):
-   return 'Hello %s!' % name
-@app.route('/pirmais')
-def index():
-   return render_template('pirmais.html')
+    return render_template('home.html')
+@app.route('/about/')
+def about():
+    return render_template('about.html')
 if __name__ == '__main__':
     app.run(debug=True)
