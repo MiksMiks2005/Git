fe927c6b70aae4741d32fe3c5d6a7485f57e2571
MiksMiks2005 <Miks.Svaheimers@edu.tukums.lv>
2025-04-02 15:35:14 +0300

    asd

--- Flaskus NIBUS/KAPECFFFFLASK.py
+++ Flaskus NIBUS/KAPECFFFFLASK.py
@@ -14,16 +14,28 @@
 
 
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+import requests
 app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('home.html')
 @app.route('/about/')
 def about():
-    return render_template('about.html')
-@app.route('/zinas/')
+    teksts = []
+    with open('Flaskus NIBUS\static\imagis\abuut.txt', 'r',encoding='utf-8') as file:
+        for line in file:
+            teksts.append(line.strip())
+    return render_template('about.html', names=teksts)
+
+@app.route('/zinas')
 def zinas():
-    return render_template('zinas.html')
+    response = requests.get("https://wttr.in/?format=3")
+    if response.status_code == 200:
+        weather_data = response.text
+        return render_template('zinas.html', teksts=weather_data)
+    else:
+        return "Nevarēja iegūt Gadalaiku"
 if __name__ == '__main__':
     app.run(debug=True)
+
--- paraugi/paraugi/p5.py
+++ paraugi/paraugi/p5.py
@@ -4,10 +4,10 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route('/vardi')
+@app.route('/')
 def show_names():
     teksts = []
-    with open('p5_teksts.txt', 'r',encoding='utf-8') as file:
+    with open('paraugi\paraugi\p5_teksts.txt', 'r',encoding='utf-8') as file:
         for line in file:
             teksts.append(line.strip())
     return render_template('p5.html', names=teksts)
