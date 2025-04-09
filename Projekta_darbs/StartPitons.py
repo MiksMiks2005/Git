a67f55d55cac669f561473ccc82d78d2fc39298f
MiksMiks2005 <Miks.Svaheimers@edu.tukums.lv>
2025-04-09 14:17:57 +0300

    adasdasdasd

--- Projekta_darbs/StartPitons.py
+++ Projekta_darbs/StartPitons.py
@@ -1,19 +1,3 @@
-# from flask import Flask , render_template
-# app = Flask(__name__)
-# @app.route('/')
-# def home():
-#     return "<html><body><h1>Hello World</h1></body></html>"
-# @app.route('/hello/')
-# def hello_name(name):
-#    return 'Hello %s!' % name
-# @app.route('/pirmais')
-# def index():
-#    return render_template('pirmais.html')
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
-
-
 from flask import Flask, render_template, request, jsonify
 import requests
 app = Flask(__name__)
