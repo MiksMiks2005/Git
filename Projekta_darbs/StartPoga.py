39f2e0dc1af74703067f6c0857e8874f848b6781
MiksMiks2005 <Miks.Svaheimers@edu.tukums.lv>
2025-04-16 14:25:42 +0300

    asdasdasd

--- Projekta_darbs/StartPoga.py
+++ Projekta_darbs/StartPoga.py
@@ -6,20 +6,10 @@ def Majas():
     return render_template('Majas.html')
 @app.route('/Par/')
 def Par():
-    teksts = []
-    with open(r'C:\Git\Projekta_darbs\abuut.txt', 'r',encoding='utf-8') as file:
-        for line in file:
-            teksts.append(line.strip())
-    return render_template('About.html', names=teksts)
-
+    return render_template('About.html')
 @app.route('/zinas')
 def zinas():
-    response = requests.get("https://wttr.in/?format=3")
-    if response.status_code == 200:
-        weather_data = response.text
-        return render_template('Palidzet.html', teksts=weather_data)
-    else:
-        return "Nevarēja iegūt Gadalaiku"
+    return render_template('Palidzet.html', teksts=weather_data)
 if __name__ == '__main__':
     app.run(debug=True)
 
